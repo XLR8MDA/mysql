6582e39533f0b5db4b7afd6c2d3548156bbe1041
XLR8090 <xlr8090@gmail.com>
2023-03-12 22:14:04 +0530

    basics_sql

--- basic_commands.sql
+++ basic_commands.sql
@@ -5,7 +5,6 @@
 --DDL Data Definition language commands are used to describe structures of tables and databases.
 --here we can create,modify,drop,rename
 
-
 --create command to create tables and databases
 create database items;
 use items;
@@ -20,14 +19,43 @@ create table house(
 alter table house
 add rooms varchar(20) not null; 
 
---drop an existing table (deletes completely)
+--drop an existing table (deletes completely the data and structure)
 drop table house;
 
---truncate the existing table (deletes the data in table structure still remains)
+--truncate the existing table (deletes the data in table but structure still remains)
 truncate table house;
 
 -- rename the existing table
 alter table house rename to building;
 
+--DML Data Manipulation Commands are used to describe the data in TABLE
+--here we can insert,update,delete the data in TABLE
+
+--insert into table
+insert into house(name,rooms) values ('AC','20'),('fridge','80'),('TV','10');
 
 
+--update to TABLE
+update house set rooms =100 where name='AC';
+
+--delete row from TABLE
+delete from house where name='AC';
+
+
+--DQL Data Query Language is used to display or look through data in the table
+-- we can select,union,join,group by
+
+--select data from table 
+select * from house;
+
+-- we can add more conditions llike where to extract specific DATABASE
+select * from house where rooms >50;
+
+--adding more data to preform union and joins
+insert into house(name,rooms) values ('AC','20'),('remote','5');
+select * from house;
+
+--union command to combine results of two tables
+select name from house 
+UNION
+select rooms from house;  --union displays data one after other not adjacently so we use joins to display side by side
