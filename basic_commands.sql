e4dee185c45e27a62e3f5d5e09a12b2bef9a6d95
XLR8090 <xlr8090@gmail.com>
2023-03-12 20:09:03 +0530

    sql_basics

--- basic_commands.sql
+++ basic_commands.sql
@@ -2,7 +2,9 @@
 
 -- DDL DML DCL TCL
 
---DDL Data Definition language commands are used to describe structures of tables and databases.ADD
+--DDL Data Definition language commands are used to describe structures of tables and databases.
+--here we can create,modify,drop,rename
+
 
 --create command to create tables and databases
 create database items;
@@ -16,4 +18,16 @@ create table house(
 
 --alter command to edit no. of columns
 alter table house
-add rooms varchar(20) not null;
+add rooms varchar(20) not null; 
+
+--drop an existing table (deletes completely)
+drop table house;
+
+--truncate the existing table (deletes the data in table structure still remains)
+truncate table house;
+
+-- rename the existing table
+alter table house rename to building;
+
+
+
